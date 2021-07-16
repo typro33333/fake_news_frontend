e066c1f62f0600edca6a20377747f1eda1e99c4c
typro33333 <hthinh3900@gmail.com>
2021-07-17 00:13:06 +0700

    no change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-export default class App extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>Hello React JS + Webpack</h1>
-            </div>
-        )
-    }
+export const App= () => {
+    return (
+      <div>
+        <h1>Hello React JS + Webpack</h1>
+      </div>
+    )
 }

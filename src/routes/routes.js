5b9ec9f74a1f786feade522a6a263344004d6bee
Thịnh Hoàng <hphuc3900@gmail.com>
2022-01-13 18:23:33 +0700

    fix router browers

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Redirect } from 'react-router-dom';
+import { Switch, Redirect, BrowserRouter } from 'react-router-dom';
 
 import RouteWithLayout from './route_layout';
 import Layout from '../@core/layout/layout';
@@ -9,15 +9,17 @@ import Search from '../page/search/search';
 
 export default function Routes() {
   return (
-    <Switch>
-      <Redirect from="/" to="/home" exact />
-      <RouteWithLayout path="/home" layout={Layout} component={Home} exact />
-      <RouteWithLayout
-        path="/search"
-        layout={Layout}
-        component={Search}
-        exact
-      />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <Redirect from="/" to="/home" exact />
+        <RouteWithLayout path="/home" layout={Layout} component={Home} exact />
+        <RouteWithLayout
+          path="/search"
+          layout={Layout}
+          component={Search}
+          exact
+        />
+      </Switch>
+    </BrowserRouter>
   );
 }
